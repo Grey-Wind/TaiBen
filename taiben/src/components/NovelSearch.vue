691bfac2a997485fbaea8dead4f64dc211540d42
Grey Wind <wjunqin7@gmail.com>
2024-09-20 12:23:01 +0800

    整体优化

--- taiben/src/components/NovelSearch.vue
+++ taiben/src/components/NovelSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>青衣台本站</h1>
-    <input class="search-box" v-model="query" @keyup.enter="search" placeholder="搜索台本..." />
+    <input class="search-box" v-model="query" @input="search" @keyup.enter="search" placeholder="搜索台本..." />
     <button class="search-button" @click="search">
       <span style="font-size: larger;">🔍</span>
     </button>
@@ -11,15 +11,17 @@
         <h3>搜索类型</h3>
         <label>
           <input type="radio" value="title" v-model="searchType" />
-          书名
+          <span>书名</span>
         </label>
         <label>
-          <input type="radio" value="author" v-model="searchType" />
-          作者
+          <input type="radio" value="author" disabled v-model="searchType" />
+          <span style="color: grey;">作者</span>
+          <span style="font-size: smaller; vertical-align: super;" title="暂不支持">ℹ</span>
         </label>
         <label>
-          <input type="radio" value="tag" v-model="searchType" />
-          标签
+          <input type="radio" value="tag" disabled v-model="searchType" />
+          <span style="color: grey;">标签</span>
+          <span style="font-size: smaller; vertical-align: super;" title="暂不支持">ℹ</span>
         </label>
       </div>
 
@@ -36,11 +38,12 @@
       </div>
     </div>
 
-    <div>
-        <ul>
-            <ResultItem v-for="result in results" :key="result._id" :result="result" />
-        </ul>
-        <p v-if="results.length === 0 && query">未找到 "{{ query }}" 的结果。</p>
+    <div class="search-results">
+      <ul>
+        <ResultItem v-for="result in results" :key="result._id" :result="result" />
+      </ul>
+      <p v-if="results.length === 0 && query">未找到 "{{ query }}" 的结果。</p>
+      <!-- <SuperMarioLoading v-if="loading" /> -->
     </div>
   </div>
 </template>
@@ -48,6 +51,7 @@
 <script>
 import axios from 'axios';
 import ResultItem from '../views/ResultItem.vue';
+// import SuperMarioLoading from '../views/SuperMarioLoading.vue';
 
 export default {
   components: {
@@ -57,20 +61,27 @@ export default {
     return {
       query: '',
       results: [],
-      searchType: 'title', // 默认搜索类型为书名
-      searchSource: 'site', // 默认搜索源为站点数据库
+      searchType: 'title',
+      searchSource: 'site',
+      // loading: false, // 添加加载状态
     };
   },
   methods: {
     async search() {
-      if (!this.query) return; // 如果没有输入查询，则不执行搜索
+      if (!this.query) {
+        this.results = [];
+        return;
+      }
 
+      // this.loading = true; // 开始加载
       try {
         axios.defaults.withCredentials = true;
         const response = await axios.get(`https://taiben-backend.qingyi-studio.top/search?query=${this.query}&source=${this.searchSource}`);
         this.results = response.data;
       } catch (error) {
         console.error('搜索失败:', error);
+      } finally {
+        // this.loading = false; // 结束加载
       }
     },
   },
@@ -98,25 +109,31 @@ export default {
   margin-left: 5px;
   position: relative;
   /* 添加定位属性 */
-  top: 0px;
+  top: 2px;
   /* 向下移动 */
 }
 
 .search-settings {
   display: flex;
-  justify-content: center; /* 居中对齐 */
-  align-items: center; /* 垂直居中 */
+  justify-content: center;
+  /* 居中对齐 */
+  align-items: center;
+  /* 垂直居中 */
   margin: 10px 0;
 }
 
-.search-options, .source-options {
+.search-options,
+.source-options {
   display: flex;
   align-items: center;
-  margin: 0 20px; /* 左右间距 */
-  text-align: center; /* 内容居中 */
+  margin: 0 20px;
+  /* 左右间距 */
+  text-align: center;
+  /* 内容居中 */
 }
 
 label {
-  margin-right: 10px; /* 单选框和标签之间的间距 */
+  margin-right: 10px;
+  /* 单选框和标签之间的间距 */
 }
 </style>
--- taiben/src/views/ResultItem.vue
+++ taiben/src/views/ResultItem.vue
@@ -1,11 +1,20 @@
 <template>
-    <li>
-        <router-link :to="{ name: 'NovelDetail', query: { id: result._id, url: result.contentUrl } }">
-            {{ result.title }}
-        </router-link>
-        <p>{{ result.description }}</p>
-        <span>by {{ result.author }}</span>
-    </li>
+    <div class="container">
+        <li class="item">
+            <h3>
+                <router-link :to="{ name: 'NovelDetail', query: { id: result._id, url: result.contentUrl } }">
+                    {{ result.title }}
+                </router-link>
+            </h3>
+            <p>
+                <span>by {{ result.author }}</span>
+            </p>
+            <p>
+                <span>简介：{{ result.description }}</span>
+            </p>
+        </li>
+        <br>
+    </div>
 </template>
 
 <script>
@@ -19,3 +28,43 @@ export default {
     name: 'ResultItem'
 };
 </script>
+
+<style lang="css" scoped>
+li {
+    left: 20%;
+    list-style: none;
+}
+
+a {
+    color: #000;
+    text-decoration: none;
+}
+
+a:hover {
+    color: grey;
+    text-decoration: underline;
+}
+</style>
+
+<style lang="css" scoped>
+.container {
+    align-items: left;
+    width: 60%;
+    /* 总宽度为100% - 左右各20% */
+    margin: 0 auto;
+    /* 上下外边距为0，左右自动 */
+    margin-left: 13%;
+    /* 向左偏移 */
+}
+
+.item {
+    text-align: left;
+    border: 2px solid darkgray;
+    border-radius: 10px;
+    background-color: rgb(247, 247, 247);
+    padding: 1% 2%;
+    padding-top: 0.5%;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
+    /* 添加阴影效果 */
+}
+</style>
